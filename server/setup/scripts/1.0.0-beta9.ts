e7c7203330b1b08e570048b10ef314b55068e466
Owen Schwartz <owen@txv.io>
2025-01-27 22:18:09 -0500

    Working on migration

--- server/setup/scripts/1.0.0-beta9.ts
+++ server/setup/scripts/1.0.0-beta9.ts
@@ -40,7 +40,6 @@ export default async function migration() {
 
     try {
         await db.transaction(async (trx) => {
-            trx.run(sql`PRAGMA foreign_keys=OFF;`);
             trx.run(sql`CREATE TABLE __new_resources (
                 resourceId integer PRIMARY KEY AUTOINCREMENT NOT NULL,
                 siteId integer NOT NULL,
@@ -58,10 +57,26 @@ export default async function migration() {
                 FOREIGN KEY (siteId) REFERENCES sites(siteId) ON UPDATE no action ON DELETE cascade,
                 FOREIGN KEY (orgId) REFERENCES orgs(orgId) ON UPDATE no action ON DELETE cascade
             );`)
-            trx.run(sql`INSERT INTO __new_resources(resourceId, siteId, orgId, name, subdomain, fullDomain, ssl, blockAccess, sso, http, protocol, proxyPort, emailWhitelistEnabled) SELECT resourceId, siteId, orgId, name, subdomain, fullDomain, ssl, blockAccess, sso, http, protocol, proxyPort, emailWhitelistEnabled FROM resources;`)
+            trx.run(sql`INSERT INTO __new_resources 
+                SELECT 
+                    resourceId,
+                    siteId,
+                    orgId,
+                    name,
+                    subdomain,
+                    fullDomain,
+                    ssl,
+                    blockAccess,
+                    sso,
+                    CAST(1 as INTEGER) as http,
+                    'tcp' as protocol,
+                    NULL as proxyPort,
+                    emailWhitelistEnabled
+                FROM resources;
+            `)
             trx.run(sql`DROP TABLE resources;`)
             trx.run(sql`ALTER TABLE __new_resources RENAME TO resources;`)
-            trx.run(sql`PRAGMA foreign_keys=ON;`)
+
             trx.run(sql`CREATE TABLE __new_targets (
                 targetId integer PRIMARY KEY AUTOINCREMENT NOT NULL,
                 resourceId integer NOT NULL,
@@ -72,7 +87,17 @@ export default async function migration() {
                 enabled integer DEFAULT true NOT NULL,
                 FOREIGN KEY (resourceId) REFERENCES resources(resourceId) ON UPDATE no action ON DELETE cascade
             );`)
-            trx.run(sql`INSERT INTO __new_targets(targetId, resourceId, ip, method, port, internalPort, enabled) SELECT targetId, resourceId, ip, method, port, internalPort, enabled FROM targets;`)
+            trx.run(sql`INSERT INTO __new_targets 
+                SELECT 
+                    targetId,
+                    resourceId,
+                    ip,
+                    method,
+                    port,
+                    internalPort,
+                    enabled
+                FROM targets;`)
+                            
             trx.run(sql`DROP TABLE targets;`)
             trx.run(sql`ALTER TABLE __new_targets RENAME TO targets;`)
         });
